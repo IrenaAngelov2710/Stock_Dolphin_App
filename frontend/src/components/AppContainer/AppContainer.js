2309224a8418e14b15cb3c715de60fa5845a6eba
IrenaAngelov <kitanovska.irena93@gmail.com>
2024-09-22 20:24:05 +0200

    render the user name only on dashboard page

--- frontend/src/components/AppContainer/AppContainer.js
+++ frontend/src/components/AppContainer/AppContainer.js
@@ -1,15 +1,30 @@
 import "./AppContainer.css";
+import React, { useContext } from "react";
+import { useLocation } from "react-router-dom";
 import Sidebar from "../Sidebar/Sidebar";
+import AuthContext from "../../utils/AuthContext";
+import userIcon from "../../assets/icons/user.svg";
 
 const AppContainer = ({ children, pageTitle }) => {
+  const { user } = useContext(AuthContext);
+  const location = useLocation();
+
+  const isDashboardPage = location.pathname === "/dashboard";
+
   return (
     <div className="app-container">
       <Sidebar />
       <div className="app-container-content">
         <div className="navbar">
-          <span className="navbar-content">{pageTitle}</span>
-          {/* koga ke ima najaveno user */}
-          {/* {user && <span>Jon Doe</span>} */}
+          <div className="navbar-content">{pageTitle}</div>
+          <div className="user-info-container">
+            {isDashboardPage && user && (
+              <>
+                <span className="user-info">Welcome back {user.name}!</span>
+                <img src={userIcon} alt="" />
+              </>
+            )}
+          </div>
         </div>
         <div className="app-container-children">{children}</div>
       </div>
