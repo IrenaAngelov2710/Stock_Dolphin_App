21be49cb4ff661cfa8c9c273cd87ec9206d0ad8f
IrenaAngelov <kitanovska.irena93@gmail.com>
2024-09-23 19:39:26 +0200

    made protected and public routes

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,4 +1,7 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom";
+import { AuthProvider } from "./utils/AuthContext";
+import ProtectedRoute from "./utils/ProtectedRoute";
+import PublicRoute from "./utils/PublicRoute";
 import Dashboard from "./screens/Dashboard/Dashboard";
 import Inventory from "./screens/Inventory/Inventory";
 import Reports from "./screens/Reports/Reports";
@@ -6,23 +9,85 @@ import Suppliers from "./screens/Suppliers/Suppliers";
 import Items from "./screens/Items/Items";
 import Orders from "./screens/Orders/Orders";
 import Login from "./screens/Login/Login";
+import Signup from "./screens/Signup/Signup";
 
 function App() {
   return (
-    <BrowserRouter>
-      <div className="App">
-        <Routes>
-          <Route path="/" element={<Login />} />
-          <Route path="/dashboard" element={<Dashboard />} />
-          <Route path="/inventory" element={<Inventory />} />
-          {/*check if should be name or categoryName */}
-          <Route path="/inventory/:categoryName/:id" element={<Items />} />
-          <Route path="/inventory/item/:itemName/:id" element={<Orders />} />
-          <Route path="/reports" element={<Reports />} />
-          <Route path="/suppliers" element={<Suppliers />} />
-        </Routes>
-      </div>
-    </BrowserRouter>
+    <AuthProvider>
+      <Router>
+        <div className="App">
+          <Routes>
+            {/* Public routes */}
+            <Route
+              path="/"
+              element={
+                <PublicRoute>
+                  <Login />
+                </PublicRoute>
+              }
+            />
+            <Route
+              path="/signup"
+              element={
+                <PublicRoute>
+                  <Signup />
+                </PublicRoute>
+              }
+            />
+            {/* Protected routes */}
+            <Route
+              path="/dashboard"
+              element={
+                <ProtectedRoute>
+                  <Dashboard />
+                </ProtectedRoute>
+              }
+            />
+            <Route
+              path="/inventory"
+              element={
+                <ProtectedRoute>
+                  <Inventory />
+                </ProtectedRoute>
+              }
+            />
+            {/*check if should be name or categoryName */}
+            <Route
+              path="/inventory/:categoryName/:id"
+              element={
+                <ProtectedRoute>
+                  <Items />
+                </ProtectedRoute>
+              }
+            />
+            <Route
+              path="/inventory/item/:itemName/:id"
+              element={
+                <ProtectedRoute>
+                  <Orders />
+                </ProtectedRoute>
+              }
+            />
+            <Route
+              path="/reports"
+              element={
+                <ProtectedRoute>
+                  <Reports />
+                </ProtectedRoute>
+              }
+            />
+            <Route
+              path="/suppliers"
+              element={
+                <ProtectedRoute>
+                  <Suppliers />
+                </ProtectedRoute>
+              }
+            />
+          </Routes>
+        </div>
+      </Router>
+    </AuthProvider>
   );
 }
 
