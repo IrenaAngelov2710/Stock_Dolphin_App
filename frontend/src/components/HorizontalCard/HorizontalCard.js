24487a81526ea2c0fa2014f545d53047ac5737c9
IrenaAngelov <kitanovska.irena93@gmail.com>
2024-10-18 00:32:48 +0200

    made toggle button in inventory page

---
+++ frontend/src/components/HorizontalCard/HorizontalCard.js
@@ -0,0 +1,120 @@
+import React, { memo, useEffect, useState, useContext } from "react";
+import "./HorizontalCard.css";
+import trashBinIcon from "../../assets/icons/trash-bin.svg";
+import { useNavigate } from "react-router-dom";
+import AuthContext from "../../utils/AuthContext";
+
+const HorizontalCard = memo(({ data, type, onDeleteClick }) => {
+  const { authToken } = useContext(AuthContext);
+  const navigate = useNavigate();
+  const [totalPrice, setTotalPrice] = useState(0);
+
+  useEffect(() => {
+    if (!authToken) {
+      console.error("No auth token available. Please log in again.");
+      return;
+    }
+
+    const fetchTotalPrice = async () => {
+      try {
+        let response;
+
+        if (type === "item") {
+          response = await fetch(
+            `http://localhost:3000/orders/item/${data._id}`,
+            {
+              method: "GET",
+              headers: {
+                Authorization: `Bearer ${authToken}`,
+                "Content-Type": "application/json",
+              },
+            }
+          );
+        } else if (type === "category") {
+          response = await fetch(
+            `http://localhost:3000/orders/category/${data._id}`,
+            {
+              method: "GET",
+              headers: {
+                Authorization: `Bearer ${authToken}`,
+                "Content-Type": "application/json",
+              },
+            }
+          );
+        }
+
+        if (!response.ok) {
+          throw new Error("Failed to fetch orders");
+        }
+
+        const result = await response.json();
+        setTotalPrice(result.totalPrice);
+      } catch (error) {
+        console.error("Error fetching orders:", error);
+      }
+    };
+
+    fetchTotalPrice();
+  }, [data._id, type, authToken]);
+
+  const formattedDate = new Intl.DateTimeFormat("en-GB", {
+    year: "numeric",
+    month: "2-digit",
+    day: "2-digit",
+    hour: "2-digit",
+    minute: "2-digit",
+    hour12: false,
+  }).format(new Date(data.updatedAt));
+
+  const slugify = (text) => {
+    return text.toString().toLowerCase().replace(/\s+/g, "-"); // Replace spaces with -
+  };
+
+  const handleClick = () => {
+    if (type === "category") {
+      navigate(`/inventory/${slugify(data.name)}/${data._id}`);
+    } else if (type === "item") {
+      navigate(`/inventory/item/${slugify(data.name)}/${data._id}`);
+    }
+  };
+  return (
+    <div key={data.id} className="horizontal-card-container">
+      <img src={`http://localhost:3000/${data?.image}`} alt="" />
+      <div className="horizontal-card-content">
+        <div className="horizontal-card-info">
+          <span className="header" onClick={handleClick}>
+            {data.name}
+          </span>
+          {/* za category prikazuvame kolku items ima */}
+          {type === "category" && (
+            <span className="info">
+              <b>{data.items?.length || "0"} Items</b> | € {totalPrice}
+            </span>
+          )}
+          {/* za items prikazuvane kolku orders ima */}
+          {type === "item" && (
+            <span className="info">
+              <b>{data.orders?.length} Purchase records</b> | € {totalPrice}
+            </span>
+          )}
+        </div>
+        <div
+          className={`horizontal-footer ${
+            type === "item" ? "item-footer" : ""
+          }`}
+        >
+          <span className="last-update">
+            Updated At:
+            <br />
+            <b>{formattedDate}</b>
+          </span>
+          <span className="horizontal-trash-bin" onClick={onDeleteClick}>
+            <img src={trashBinIcon} alt="" />
+          </span>
+        </div>
+      </div>
+    </div>
+  );
+});
+
+export default HorizontalCard;
